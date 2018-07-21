5f64064d231a9698bebc9f06b3520adb5349cd56
Vic Luo <vicluo96@gmail.com>
2018-07-21 17:22:43 +0800

    check-no-und-symbols: fix library path

--- check-no-und-symbols.sh
+++ check-no-und-symbols.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-cc -nostdlib -nodefaultlibs -lgcc -nostdinc -fno-use-linker-plugin -fuse-ld=gold -Wl,--whole-archive -r -o /tmp/a.o build/libd-rlib.a
+cc -nostdlib -nodefaultlibs -lgcc -nostdinc -fno-use-linker-plugin -fuse-ld=gold -Wl,--whole-archive -r -o /tmp/a.o build/librasterizer-d-embed.a
 if nm --undefined-only /tmp/a.o | grep 'U' | egrep -v '_start|_GLOBAL_OFFSET_TABLE_'; then
     echo "found missing symbols..."
     exit 1
