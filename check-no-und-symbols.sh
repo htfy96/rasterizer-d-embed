1782c52f0d7625f52e8700584b90baa67cf15807
Vic Luo <vicluo96@gmail.com>
2018-07-21 17:27:53 +0800

    check-no-und-symbols: allow more symbols

--- check-no-und-symbols.sh
+++ check-no-und-symbols.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 cc -nostdlib -nodefaultlibs -lgcc -nostdinc -fno-use-linker-plugin -fuse-ld=gold -Wl,--whole-archive -r -o /tmp/a.o build/librasterizer-d-embed.a
-if nm --undefined-only /tmp/a.o | grep 'U' | egrep -v '_start|_GLOBAL_OFFSET_TABLE_'; then
+if nm --undefined-only /tmp/a.o | grep 'U' | egrep -vw '_start|_GLOBAL_OFFSET_TABLE_|__assert|memset|memcmp|memcpy|_memset[[:alnum:]]+'; then
     echo "found missing symbols..."
     exit 1
 else
